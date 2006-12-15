077a4aee79f62308b98ba0da608b3dfe65795df2
iovar <iovar@f606c939-3180-4ac9-a4b8-4b8779d57d0a>
2006-12-15 19:24:18 +0000

    made sure that sound capture will wait for the first time expiration,before it starts(wait on cond_var before loop).
    git-svn-id: https://svn.code.sf.net/p/recordmydesktop/svn/trunk@238 f606c939-3180-4ac9-a4b8-4b8779d57d0a

--- recordmydesktop/src/capture_sound.c
+++ recordmydesktop/src/capture_sound.c
@@ -31,9 +31,13 @@ void *CaptureSound(ProgData *pdata){
 
     int frames=pdata->periodsize;
     int framesize=((snd_pcm_format_width(SND_PCM_FORMAT_S16_LE))/8)*pdata->args.channels;
-    pthread_mutex_t pmut;
+    pthread_mutex_t pmut,tmut;
     pthread_mutex_init(&pmut,NULL);
+    pthread_mutex_init(&tmut,NULL);
 
+    //start capturing only when everything else is finsihed
+    //ideally, at the same time that the first frame is captured
+    pthread_cond_wait(&pdata->time_cond,&tmut);
     while(pdata->running){
         int sret=0;
         SndBuffer *newbuf,*tmp;
