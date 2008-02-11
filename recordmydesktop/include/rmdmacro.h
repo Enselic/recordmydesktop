899e736fa1baa9b4151fce9fff0aa1852fe41046
iovar <iovar@f606c939-3180-4ac9-a4b8-4b8779d57d0a>
2008-02-11 17:42:35 +0000

    added year 2008 in all copyright notices
    git-svn-id: https://svn.code.sf.net/p/recordmydesktop/svn/trunk@494 f606c939-3180-4ac9-a4b8-4b8779d57d0a

--- recordmydesktop/include/rmdmacro.h
+++ recordmydesktop/include/rmdmacro.h
@@ -2,7 +2,7 @@
 *                            recordMyDesktop                                  *
 *******************************************************************************
 *                                                                             *
-*            Copyright (C) 2006,2007 John Varouhakis                          *
+*            Copyright (C) 2006,2007,2008 John Varouhakis                     *
 *                                                                             *
 *                                                                             *
 *   This program is free software; you can redistribute it and/or modify      *
