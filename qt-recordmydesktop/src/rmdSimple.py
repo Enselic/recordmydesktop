39ecf11579f95b086390dd57fcefa88f1a7f096f
iovar <iovar@f606c939-3180-4ac9-a4b8-4b8779d57d0a>
2008-11-22 10:58:07 +0000

    Changed qt-recordMyDesktop's license to GPL version 3 (or later). All license headers have been changed to reflect this change and COPYING.gpl3 has been added with the full text of the license. Also, the previous record/pause/stop icons and their source svg's have been removed and replaced with equivalents from the Oxygen project, provided under the GNU-LGPL version 3 (or later). COPYING.lgpl3 and COPYING.icons have been added to provide the neccessary information. Makefile.am and src/Makefile.am have been modified to reflect this change in the project files. Version changed in configure.ac, to 0.3.8-cvs
    git-svn-id: https://svn.code.sf.net/p/recordmydesktop/svn/trunk@578 f606c939-3180-4ac9-a4b8-4b8779d57d0a

--- qt-recordmydesktop/src/rmdSimple.py
+++ qt-recordmydesktop/src/rmdSimple.py
@@ -2,23 +2,21 @@
 #*                       qt-recordMyDesktop                                    *
 #*******************************************************************************
 #*                                                                             *
-#*           Copyright (C) 2007  John Varouhakis                               *
+#*           Copyright (C) 2007, 2008  John Varouhakis                         *
 #*                                                                             *
 #*                                                                             *
-#*  This program is free software; you can redistribute it and/or modify       *
-#*  it under the terms of the GNU General Public License as published by       *
-#*  the Free Software Foundation; either version 2 of the License, or          *
-#*  (at your option) any later version.                                        *
+#*   This program is free software: you can redistribute it and/or modify      *
+#*   it under the terms of the GNU General Public License as published by      *
+#*   the Free Software Foundation, either version 3 of the License, or         *
+#*   (at your option) any later version.                                       *
 #*                                                                             *
-#*  This program is distributed in the hope that it will be useful,            *
-#*  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
-#*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
-#*  GNU General Public License for more details.                               *
+#*   This program is distributed in the hope that it will be useful,           *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+#*   GNU General Public License for more details.                              *
 #*                                                                             *
-#*  You should have received a copy of the GNU General Public License          *
-#*  along with this program; if not, write to the Free Software                *
-#*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  *
-#*                                                                             *
+#*   You should have received a copy of the GNU General Public License         *
+#*   along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
 #*                                                                             *
 #*                                                                             *
 #*  For further information contact me at johnvarouhakis@gmail.com             *
@@ -116,7 +114,7 @@ class simpleWidget(object):
         self.NETABox.insertWidget(-1,self.advanced_button)
         self.NETABox.insertWidget(-1,self.tipLabel)
         style=QtGui.QApplication.style()
-        icon_rec_pxm=QtGui.QPixmap(os.path.join(rmdConfig.pixmapdir,"qt-recordmydesktop_record.png"))
+        icon_rec_pxm=QtGui.QPixmap(os.path.join(rmdConfig.pixmapdir,"media-record.svg"))
 
         self.win_button=QtGui.QPushButton(smplButtonStrings[1],self.window)
         self.start_button=QtGui.QPushButton(QtGui.QIcon(icon_rec_pxm),smplStrings['Record'],self.window)
--- qt-recordmydesktop/src/rmdStrings.py
+++ qt-recordmydesktop/src/rmdStrings.py
@@ -1,3 +1,28 @@
+#/******************************************************************************
+#*                       qt-recordMyDesktop                                    *
+#*******************************************************************************
+#*                                                                             *
+#*           Copyright (C) 2007, 2008  John Varouhakis                         *
+#*                                                                             *
+#*                                                                             *
+#*   This program is free software: you can redistribute it and/or modify      *
+#*   it under the terms of the GNU General Public License as published by      *
+#*   the Free Software Foundation, either version 3 of the License, or         *
+#*   (at your option) any later version.                                       *
+#*                                                                             *
+#*   This program is distributed in the hope that it will be useful,           *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+#*   GNU General Public License for more details.                              *
+#*                                                                             *
+#*   You should have received a copy of the GNU General Public License         *
+#*   along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+#*                                                                             *
+#*                                                                             *
+#*  For further information contact me at johnvarouhakis@gmail.com             *
+#******************************************************************************/
+
+
 from PyQt4 import QtGui,QtCore
 import rmdConfig
 import locale, gettext
