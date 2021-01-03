77fa6debf42ee3af45f2101c5d1d0ecf3ac4fd94
Martin Nordholts <enselic@gmail.com>
2021-01-03 08:01:27 +0100

    Cast AllPlanes to CARD32 for planeMask
    Fixes:
    warning: conversion from ‘long unsigned int’ to ‘CARD32’ {aka ‘unsigned int’} changes value from ‘18446744073709551615’ to ‘4294967295’ [-Woverflow]

--- recordmydesktop/src/rmd_getzpixmap.c
+++ recordmydesktop/src/rmd_getzpixmap.c
@@ -55,7 +55,7 @@ int rmdGetZPixmap(Display *dpy,
     request->y=y;
     request->width=width;
     request->height=height;
-    request->planeMask=AllPlanes;
+    request->planeMask=(CARD32)AllPlanes;
     request->format=ZPixmap;
     if((!_XReply(dpy,(xReply *)&reply,0,xFalse))||(!reply.length)){
         UnlockDisplay(dpy);
@@ -94,7 +94,7 @@ int rmdGetZPixmapSHM(Display *dpy,
     request->y=y;
     request->width=width;
     request->height=height;
-    request->planeMask=AllPlanes;
+    request->planeMask=(CARD32)AllPlanes;
     request->format=ZPixmap;
     request->offset=data-shminfo->shmaddr;
 
